e45ba7b074507e6402e40e1f8075e33f9fdc7316
Kunnaspe <paul.eric.kunnas@gmail.com>
2026-07-31 00:56:07 -0400

    Fix Python and JavaScript linting errors for CI/CD workflow

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,3 +11,27 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+
+# CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
+
+
+# CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')
+
+
+# CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+
+
+# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
